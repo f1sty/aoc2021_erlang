8adacf116f07c2adf0447eef39a7cc53e5544c69
Yurii Skrynnykov <truef1s7@gmail.com>
2023-09-19 12:10:14 +0300

    Add helper function print_result

--- src/aoc2021_erlang.erl
+++ src/aoc2021_erlang.erl
@@ -1,3 +1,7 @@
 -module(aoc2021_erlang).
 
--export([]).
+-export([print_result/3]).
+
+-spec print_result(pos_integer(), pos_integer(), any()) -> ok.
+print_result(Day, Part, Result) ->
+  io:format("[day ~p] part ~p: ~p~n", [Day, Part, Result]).
