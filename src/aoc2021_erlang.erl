6ce3a90655a7a460e619d908232a4fb56b149609
Yurii Skrynnykov <truef1s7@gmail.com>
2023-09-19 13:33:59 +0300

    Add convenient function run_all/0 to main module

--- src/aoc2021_erlang.erl
+++ src/aoc2021_erlang.erl
@@ -1,7 +1,11 @@
 -module(aoc2021_erlang).
 
--export([print_result/3]).
+-export([run_all/0, print_result/3]).
 
 -spec print_result(pos_integer(), pos_integer(), any()) -> ok.
 print_result(Day, Part, Result) ->
   io:format("[~p] ~p: ~p~n", [Day, Part, Result]).
+
+run_all() ->
+  day1:run(),
+  day2:run().
