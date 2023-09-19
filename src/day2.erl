7a53c1673c60f543c4b8e97ddc4bcdbdf2da5918
Yurii Skrynnykov <truef1s7@gmail.com>
2023-09-19 13:31:59 +0300

    Add day 2 part 2 solution

--- src/day2.erl
+++ src/day2.erl
@@ -24,7 +24,22 @@ part1() ->
   ?RESULT.
 
 part2() ->
-  undefined.
+  List = read_input("inputs/day2"),
+  {HorizontalPosition, Depth, _Aim} =
+    lists:foldl(fun(Command, {HorizontalPosition, Depth, Aim}) ->
+                   case Command of
+                     <<"forward ", _X/binary>> ->
+                       X = binary_to_integer(_X),
+                       {HorizontalPosition + X, Depth + Aim * X, Aim};
+                     <<"up ", Y/binary>> -> {HorizontalPosition, Depth, Aim - binary_to_integer(Y)};
+                     <<"down ", Y/binary>> ->
+                       {HorizontalPosition, Depth, Aim + binary_to_integer(Y)}
+                   end
+                end,
+                {0, 0, 0},
+                List),
+  Result = HorizontalPosition * Depth,
+  ?RESULT.
 
 -spec read_input(string()) -> [binary()].
 read_input(FileName) ->
